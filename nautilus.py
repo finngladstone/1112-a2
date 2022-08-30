d16647cc15a2e9fe583d83c3b965b959859f4917
finngladstone <finngladstone9@gmail.com>
2022-08-30 19:00:28 +1000

    Added ood

--- nautilus.py
+++ nautilus.py
@@ -1,5 +1,60 @@
+"""Task 1"""
+
+def exit(): 
+    pass 
+
+def pwd(): 
+    pass 
+
+def cd(directory):
+    pass 
+
+def mkdir(p, directory):
+    # default drwxr -x
+    pass 
+
+def touch(filename):
+    # default -rw-r--
+    pass 
+
+"""Task 2"""
+
+def cp(source, destination):
+    pass 
+
+def mv(source, destination):
+    pass 
+
+def rm(path):
+    pass 
+
+def rmdir(directory):
+    pass 
+
+"""Task 3"""
+
+def chmod(r, perms, path):
+    pass 
+
+def chown(r, user, path):
+    pass 
+
+def adduser(user):
+    pass 
+
+def deluser(user):
+    pass 
+
+def su(user): # Change current effective user 
+    pass 
+
+def ls(a, d, l, path):
+    pass 
+
+
+
 def main():
-    # TODO
+    # Need cmdline parser 
     pass
 
 
