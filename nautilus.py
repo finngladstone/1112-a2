5592ce9f07d0f0b74ef1d07821f4e679832459f2
finngladstone <finngladstone9@gmail.com>
2022-09-07 13:17:23 +1000

    rewrote class definitions and functions

--- nautilus.py
+++ nautilus.py
@@ -1,174 +1,90 @@
-"""Classes"""
-
-# create home directoy upon execution? spec
-# creat root user upon exec
+from tokenize import Name
 
 
-class User:
+class User
 
-    Users = [] # class attributes - instance defined within __init__
-    
-    def __init__(self, name, root=False) -> None:
+    def __init__(self, name, root=False, currentDir=None) -> None:
         self.name = name
         self.root = root
-        self.current_dir = None
-        self.__class__.Users.append(self)
+        self.currentDir = currentDir
+        self.perms = {}
+
+    def updateCurrentDir(self, dir):
+        self.currentDir = dir
+        
     
-    def remove(self):
-        self.__class__.Users.remove(self)
-        del self # check functionality 
+    def exit(self):
+        print("bye, {}".format())
 
-    def update_dir(self, dir):
-        self.current_dir = dir
+    def pwd(self):
+        print(self.currentDir)
+
+    def mkdir(self, dir, p=None):
+        pass 
+
+    def touch(self, name):
+        pass 
+
+    def cp(self, source, destination):
+        pass 
+
+    def mv(self, source, destination):
+        pass 
+
+    def rm(self, path):
+        pass 
+
+    def chmod(self, path, perms, r=None):
+        pass 
+
+    def chown(self, path, user, r=None):
+        pass 
+
+    def adduser(self, user):
+        pass 
+
+    def deluser(self, user):
+        pass 
+
+    def su(self, user):
+        pass 
+
+    def ls(self, path=None, l=None, d=None, a=None):
+        pass 
 
 
-class Directory: 
+class Directory:
 
-    Directories = [] # class attributes
-
-    def __init__(self, name, parent, user=None) -> None:
+    def __init__(self, name, parent=None) -> None:
         self.name = name 
-
         self.parent = parent 
-        self.subdir = []
+        self.subdirs = []
         self.files = []
+    
 
-        self.owner = user
+class File:
 
-        self.__class__.Directories.append(self)
-
-    def remove(self):
-        self.__class__.Users.remove(self)
-        del self
-
-    def changeOwner(self, newOwner):
-        self.owner = newOwner
-
-    def addSubdir(self, dir): 
-        self.subdir.append(dir)
-
-    def removeSubdir(self, dir):
-        self.subdir.remove(dir)
-
-    def addFile(self, file):
-        self.files.append(file)
-
-    def removeFile(self, file): 
-        self.files.remove(file)
-     
-
-class File: 
-
-    Files = []
-
-    def __init__(self, name, user, perms) -> None:
-        self.name = name
-        self.owner = user
-        self.perms = perms
-
-        self.__class__.Files.append(self)
-
-    def remove(self):
-        self.__class__.Files.remove(self)
-        del self 
-
-    def changeOwner(self, newOwner):
-        self.owner = newOwner
-
-    """
-    permissions for self 
-    reference to its directory? 
-    metadata (owner)
-    """
+    def __init__(self, name) -> None:
+        self.name = name 
     pass 
 
-"""Task 1"""
+class Namespace:
 
-def exit(): 
-    print("bye, {}".format(curr_user))
-    pass 
-
-def pwd(): 
-    print(curr_dir) 
-
-def cd(cwd, targetwd):
-    # if target exists
-    #   if perms match
-    #       cd to targetdir
-    pass 
-
-def mkdir(directory, p=None):
-    # if p == None and ancestor_fail:
-    #   print("mkdir: Ancestor directory does not exist")
-    # default drwxr -x
-    # if dir == None: raise invalidinput
-    pass 
-
-def touch(filename):
-    # default -rw-r--
-    pass 
-
-"""Task 2"""
-
-def cp(source, destination):
-    pass 
-
-def mv(source, destination):
-    pass 
-
-def rm(path):
-    pass 
-
-def rmdir(directory):
-    pass 
-
-"""Task 3"""
-
-def chmod(r, perms, path):
-    pass 
-
-def chown(r, user, path):
-    pass 
-
-def adduser(user):
-    # must be root 
-    pass 
-
-def deluser(user):
-    pass 
-
-def su(user): # Change current effective user 
-    pass 
-
-def ls(a, d, l, path):
-    pass 
-
-
+    def __init__(self, root) -> None:
+        self.root = root 
+        self.files = []
+        self.dirs = []
+    
 
 def main():
 
-    fn_dic = {"exit":exit, "pwd":pwd, "cd":cd, "mkdir":mkdir,
-        "touch":touch, "cp":cp}
+    rootDir = Directory("root", None)
+    rootUser = User("root", True, rootDir)
 
-    root = User("root", True)
-    root_dir = Directory("/", None, None)
+    namespace = Namespace(rootDir) # do i really need this 
 
-    current_user = root 
-    root.update_dir(root_dir)
+
     
-
-
-    while True:
-        start_line = "{}:{}$ ".format(curr_user.name, curr_dir.name)
-        keyboard = input(start_line)
-
-        fn_dic[keyboard]()
-        
-
-
-
-        
-
 
 
 if __name__ == '__main__':
